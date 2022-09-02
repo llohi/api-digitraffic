2e65a5aa96053ca53852ca48bfd20edbba53b3fe
Joose Lohi <joose.lohi01@gmail.com>
2022-09-02 05:18:35 +0300

    add DigiURL.java

---
+++ ApiDigitraffic/src/main/java/DigiURL.java
@@ -0,0 +1,9 @@
+/**
+ * This class contains methods to generate an url for the Digitraffic API.
+ * @author J.L.
+ */
+public class DigiURL {
+
+    private final String ORIGIN = "https://tie.digitraffic.fi/api/maintenance/v1/tracking/";
+
+}
