6ae6ba514ccc3cabc93f7badf58b01051905944d
Joose Lohi <joose.lohi01@gmail.com>
2022-09-05 16:43:38 +0300

    cleanup code

--- ApiDigitraffic/src/main/java/Main.java
+++ ApiDigitraffic/src/main/java/Main.java
@@ -1,21 +1,9 @@
-import com.google.gson.reflect.TypeToken;
-
-import java.io.IOException;
-import java.util.*;
-
 /**
  * Class used to test code.
  * @author J.L.
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-        List<Task> tasks = ServerRequest.getObject(
-                DigiURL.MAINTENANCE_ALL, new TypeToken<List<Task>>() {}
-        );
-
-        for (Task task : tasks) {
-            System.out.println(task.getNameEn());
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
