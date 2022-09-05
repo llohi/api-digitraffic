fb28b59acdd5c80286f3c3e11f8d5863f3c3c647
Joose Lohi <joose.lohi01@gmail.com>
2022-09-05 14:59:30 +0300

    add class Task.java

---
+++ ApiDigitraffic/src/main/java/Task.java
@@ -0,0 +1,36 @@
+public class Task {
+
+    private String id, nameFi, nameEn, nameSv;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNameFi() {
+        return nameFi;
+    }
+
+    public void setNameFi(String nameFi) {
+        this.nameFi = nameFi;
+    }
+
+    public String getNameEn() {
+        return nameEn;
+    }
+
+    public void setNameEn(String nameEn) {
+        this.nameEn = nameEn;
+    }
+
+    public String getNameSv() {
+        return nameSv;
+    }
+
+    public void setNameSv(String nameSv) {
+        this.nameSv = nameSv;
+    }
+}
