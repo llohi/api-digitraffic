48e3e73413f880ba6f0f48f6da71235244ec3931
Joose Lohi <joose.lohi01@gmail.com>
2022-09-05 12:19:20 +0300

    add url generation methods to DigiURL.java

--- ApiDigitraffic/src/main/java/DigiURL.java
+++ ApiDigitraffic/src/main/java/DigiURL.java
@@ -4,13 +4,63 @@
  */
 public class DigiURL {
 
-    private final String MAINTENANCE = "https://tie.digitraffic.fi/api/maintenance/v1/tracking/";
-    private final String CONDITIONS = "https://tie.digitraffic.fi/api/v3/data/road-conditions/";
-    private final String MESSAGES = "https://tie.digitraffic.fi/api/traffic-message/v1/";
+    static final String MAINTENANCE_ROOT = "https://tie.digitraffic.fi/api/maintenance/v1/tracking/";
+    static final String CONDITIONS_ROOT = "https://tie.digitraffic.fi/api/v3/data/road-conditions/";
+    static final String TRAFFIC_ROOT = "https://tie.digitraffic.fi/api/traffic-message/v1/";
+
+    static final String MAINTENANCE_ALL = "https://tie.digitraffic.fi/api/maintenance/v1/tracking/tasks";
+
 
     static String makeURL() {
         String url = "";
         return url;
+    }
+
+    /**
+     * Generate an url to get road maintenance tracking tasks
+     * for a specific area.
+     * @param start_time the "minimum" starting time for the tasks
+     * @param end_time the "deadline" for the tasks
+     * @param x_min the minimum x coordinate
+     * @param y_min the minimum y coordinate
+     * @param x_max the maximum x coordinate
+     * @param y_max the maximum y coordinate
+     * @param task_name the name of the task (if empty, all tasks)
+     * @return the formatted url
+     */
+    static String maintenanceURL(String start_time, String end_time,
+                                 int x_min, int y_min, int x_max, int y_max,
+                                 String task_name) {
+        return MAINTENANCE_ROOT +
+                "routes?endFrom="+start_time+"&endBefore"+end_time+
+                "&xMin="+x_min+"&yMin="+y_min+"&xMax="+x_max+"&yMax="+y_max+
+                "&taskId="+task_name+"&domain=state-roads";
+    }
+
+    /**
+     * Generate an url to get the road conditions for a specific area.
+     * @param x_min the minimum x coordinate
+     * @param y_min the minimum y coordinate
+     * @param x_max the maximum x coordinate
+     * @param y_max the maximum y coordinate
+     * @return the formatted url
+     */
+    static String conditionsURL(int x_min, int y_min,
+                                int x_max, int y_max) {
+        return CONDITIONS_ROOT +
+                x_min+"/"+y_min+"/"+x_max+"/"+y_max;
+    }
+
+    /**
+     * Generate an url to get traffic messages according to the situation type.
+     * @param situation_type the type of the situation
+     * @return the formatted url
+     */
+    static String trafficURL(String situation_type) {
+
+        return TRAFFIC_ROOT +
+                "messages?inactiveHours=0&includeAreaGeometry=false" +
+                "&situationType="+situation_type;
     }
 
 }
